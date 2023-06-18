56c3bf6d37bce1b4bf0216b2a80c2a7276589e03
Harlet Orellan Urbina (She/Her) <116825162+harlet17@users.noreply.github.com>
2023-06-18 16:36:35 +0000

    Process stdout stdin

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const questions = [
-    "1.- O Que aprendi hoje?: ",
-    "1.- Quantas pessoas ajudei hoje?: "
+    "1.- Qual é seu nome: ",
+    "2.- Qual é sua idade: "
 ]
 const ask = (index=0)=>{
-    process.stdout.write(questions[index] + '\n')
+    process.stdout.write('\n' + questions[index])
 }
 
 ask()
+
+const answers = []
+
+process.stdin.on("data",data =>{
+    answers.push(data.toString().trim()+'\n')
+    if (answers.length < questions.length)
+        ask(answers.length)
+    else{
+        process.stdout.write('\n\n' + answers)
+        process.exit()
+    }
+})
